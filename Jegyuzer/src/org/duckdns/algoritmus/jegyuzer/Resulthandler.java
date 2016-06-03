4bea1e95048e7d2ea3f9147902bb19a96e5a8d6c
attila <attila@attila.com>
2016-06-03 14:19:21 +0200

    Change data fetching to return Json

--- Jegyuzer/src/org/duckdns/algoritmus/jegyuzer/Resulthandler.java
+++ Jegyuzer/src/org/duckdns/algoritmus/jegyuzer/Resulthandler.java
@@ -3,8 +3,6 @@ package org.duckdns.algoritmus.jegyuzer;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
-import java.util.List;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -16,17 +14,13 @@ public class Resulthandler {
 	private static final String dbURL="jdbc:mysql://192.168.0.21:3306/jegyuzer";
 	private static Connection conn = null;
     private static Statement stmt = null;
-    //private static final String HTML_START="<html><body>";
-    //private static final String HTML_END="</body></html>";
     private static final String tableName="results";
 	
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String showRequests(){
 		createConnection();
-		List<List<String>> result = Utils.selectStarRequests(conn,stmt,tableName);
-		return Utils.generateTable(result,"RESULTS");
-		
+		return Utils.getAllResultsAsJson(conn,stmt,tableName);
 	}
 	
 	private void createConnection() {
