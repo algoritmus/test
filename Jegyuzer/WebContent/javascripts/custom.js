2b05721b594966243f0b180febd11bea69c58411
attila <attila@attila-virtual-machine>
2016-06-10 18:46:18 +0200

    put javascript to separate file

---
+++ Jegyuzer/WebContent/javascripts/custom.js
@@ -0,0 +1,37 @@
+$.ajaxSetup ({
+    // Disable caching of AJAX responses
+    cache: false
+});
+$(document).ready(function(){
+	//$("#result-table").hide();
+	$("#Results").click(function(){
+		$.ajax({
+			url:"./ju/result",
+			async:false,
+			success:function(data){
+				$("#result-table").dynatable({
+					  dataset: {
+					    records: data
+					  }}
+				  );
+				  $("#result-table").show();
+				},
+		});
+				
+	});
+});
+
+function openCity(evt, page) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(page).style.display = "block";
+    evt.currentTarget.className += " active";
+    
+}
