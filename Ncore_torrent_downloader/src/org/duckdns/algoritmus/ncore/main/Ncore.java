707b4f9f97f366d9a5f576b06ddb37957151931b
ATDESY <ATDESY@CH-40-02-16-016.corp.capgemini.com>
2017-05-16 20:54:40 +0200

    add input file

--- Ncore_torrent_downloader/src/org/duckdns/algoritmus/ncore/main/Ncore.java
+++ Ncore_torrent_downloader/src/org/duckdns/algoritmus/ncore/main/Ncore.java
@@ -2,8 +2,11 @@ package org.duckdns.algoritmus.ncore.main;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -24,7 +27,11 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class Ncore {
 	
-	public static Connection conn;
+	static Connection conn;
+	static String keyword;
+	static String type;
+	static boolean getAll;
+	static String key;
 	public static void main(String[] args) {
 		
 		
@@ -45,40 +52,39 @@ public class Ncore {
         WebDriver driver = new HtmlUnitDriver();
         
         
-    	BufferedReader br = null;
-		BufferedWriter bw = null;
+    	
+		
     	
 
     	try{
-			
-
-    		
+    		readSearchItems();
+   		
 			conn = DBUtils.connectionToDerby(null);
 			
 			DBUtils.dropTable(conn, dropTable);
 			DBUtils.createTable(conn);
 			
-    		String key = NcoreUtils.login(driver, "Algoritmus", "NCorejelszo123");
-			NcoreUtils.findNewTorrents(driver, conn, key, "lucifer s02 720", "hdser", false);
-
-			
-			
-
-			
-			
-			
-			
-			
+    		key = NcoreUtils.login(driver, "Algoritmus", "NCorejelszo123");
+			NcoreUtils.findNewTorrents(driver, conn, key, keyword, type, getAll);
 	      
 		}catch(Exception e){
 			e.printStackTrace();
 		}finally{
-	        
-			//driver.close();
-	        
+        
 	        driver.quit();
 	        long duration = (System.currentTimeMillis() - start) / 1000;
 	        System.out.println("Running time: " + duration + "s");
+		}
+	}
+	private static void readSearchItems() throws FileNotFoundException, IOException {
+		BufferedReader br = new BufferedReader(new FileReader("torrents.csv"));
+		String line = null;
+		while (( line = br.readLine()) != null){
+			keyword = line.split(",")[0];
+			type = line.split(",")[0];
+			getAll = Boolean.parseBoolean(line.split(",")[0]);
+			
+			
 		}
 	}
 
